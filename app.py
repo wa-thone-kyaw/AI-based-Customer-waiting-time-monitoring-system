d2ce9c632530f3edf83040a613f75ce2246d369e
Wa Thone Kyaw <62678144+wa-thone-kyaw@users.noreply.github.com>
2024-07-31 20:12:17 +0630

    forget to push

--- app.py
+++ app.py
@@ -17,10 +17,12 @@ socketio = SocketIO(app)
 USERNAME = "admin"
 PASSWORD = "password"
 
+
 # Routes
 @app.route("/")
 def home():
     return render_template("login.html")
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -31,23 +33,30 @@ def login():
     else:
         return redirect(url_for("home"))
 
+
 @app.route("/index")
 def index():
     return render_template("index.html")
 
+
 @app.route("/video_feed")
 def video_feed():
-    return Response(detect_people(), mimetype="multipart/x-mixed-replace; boundary=frame")
+    return Response(
+        detect_people(), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
+
 
 @app.route("/waiting_times")
 def waiting_times():
     return jsonify(get_waiting_times())
+
 
 @app.route("/stop_all_alarms", methods=["POST"])
 def stop_all_alarms():
     for person_info in detected_people.values():
         person_info["alarm_triggered"] = False
     return jsonify({"message": "All alarms stopped"}), 200
+
 
 @app.route("/settings", methods=["GET", "POST"])
 def settings():
@@ -57,34 +66,39 @@ def settings():
         setTime = float(data.get("setTime", setTime))
     return jsonify({"setTime": setTime})
 
+
 @app.route("/items")
 def items():
     items = {
         "cakes": ["Chocolate Cake", "Vanilla Cake", "Strawberry Cake"],
-        "coffee": ["Espresso", "Latte", "Cappuccino"]
+        "coffee": ["Espresso", "Latte", "Cappuccino"],
     }
     return jsonify(items)
+
 
 @app.route("/orders", methods=["POST"])
 def orders():
     data = request.json
     person_id = int(data.get("person_id"))
     order = data.get("order")
-    order_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    order_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     if person_id in detected_people:
         person_info = detected_people[person_id]
         if "orders" not in person_info:
             person_info["orders"] = []
         person_info["orders"].append({"item": order, "time": order_time})
-        
+
         # Reset the waiting time when an order is placed
         person_info["time"] = time.time()
         person_info["last_update_time"] = time.time()
-        
+
         print(f"Order received for Person {person_id}: {order} at {order_time}")
     else:
         print(f"Person {person_id} not found.")
-    return jsonify({"message": "Order placed successfully", "person_id": person_id, "order": order})
+    return jsonify(
+        {"message": "Order placed successfully", "person_id": person_id, "order": order}
+    )
+
 
 # YOLOv8 model loading
 model = YOLO("yolov8l.pt")
@@ -98,9 +112,16 @@ colors_dict = {}
 
 # Predefined colors
 strong_colors = [
-    (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0),
-    (255, 0, 255), (0, 255, 255), (128, 0, 128), (0, 128, 128),
-    (128, 128, 0), (255, 165, 0)
+    (255, 0, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+    (255, 255, 0),
+    (255, 0, 255),
+    (0, 255, 255),
+    (128, 0, 128),
+    (0, 128, 128),
+    (128, 128, 0),
+    (255, 165, 0),
 ]
 
 # Frame resizing
@@ -114,10 +135,12 @@ alarm_sound = None
 
 if os.path.isfile(alarm_file):
     import pygame
+
     pygame.init()
     alarm_sound = pygame.mixer.Sound(alarm_file)
 else:
     print(f"Warning: File '{alarm_file}' not found. Alarm functionality is disabled.")
+
 
 def update_waiting_times():
     while True:
@@ -129,29 +152,37 @@ def update_waiting_times():
         socketio.emit("update_waiting_times", get_waiting_times())
         time.sleep(1)
 
+
 def get_waiting_times():
     waiting_times = {}
     current_time = time.time()
     for person_info in detected_people.values():
         if "orders" in person_info:
             elapsed_time = current_time - person_info["time"]
-            waiting_time_str = time.strftime('%H:%M:%S', time.gmtime(elapsed_time))
+            waiting_time_str = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
             if elapsed_time > setTime:
                 waiting_time_str += " (EXCEEDED)"
                 if alarm_sound:
                     if not person_info["alarm_triggered"]:
                         alarm_sound.play()
                         person_info["alarm_triggered"] = True
-            orders_str = ', '.join([f"{order['item']} ({order['time']})" for order in person_info.get("orders", [])])
+            orders_str = ", ".join(
+                [
+                    f"{order['item']} ({order['time']})"
+                    for order in person_info.get("orders", [])
+                ]
+            )
             waiting_times[f"Person {person_info['id']}"] = {
                 "waiting_time": waiting_time_str,
-                "orders": orders_str
+                "orders": orders_str,
             }
     return waiting_times
+
 
 update_thread = threading.Thread(target=update_waiting_times)
 update_thread.daemon = True
 update_thread.start()
+
 
 def detect_people():
     global person_id_counter
@@ -173,7 +204,9 @@ def detect_people():
                     matched_person_id = None
                     for person_id, person_info in detected_people.items():
                         last_centroid = person_info["centroid"]
-                        distance = np.linalg.norm(np.array(centroid) - np.array(last_centroid))
+                        distance = np.linalg.norm(
+                            np.array(centroid) - np.array(last_centroid)
+                        )
                         if distance < 50:
                             matched_person_id = person_id
                             break
@@ -186,30 +219,48 @@ def detect_people():
                             "time": time.time(),
                             "last_update_time": time.time(),
                             "alarm_triggered": False,
-                            "orders": []
+                            "orders": [],
                         }
-                        colors_dict[person_id_counter] = strong_colors[person_id_counter % len(strong_colors)]
+                        colors_dict[person_id_counter] = strong_colors[
+                            person_id_counter % len(strong_colors)
+                        ]
                         person_id_counter += 1
                     else:
-                        detected_people[matched_person_id].update({
-                            "centroid": centroid,
-                            "bbox": (x1, y1, x2 - x1, y2 - y1),
-                            "last_update_time": time.time(),
-                            "alarm_triggered": False
-                        })
+                        detected_people[matched_person_id].update(
+                            {
+                                "centroid": centroid,
+                                "bbox": (x1, y1, x2 - x1, y2 - y1),
+                                "last_update_time": time.time(),
+                                "alarm_triggered": False,
+                            }
+                        )
 
         for person_info in detected_people.values():
             if "orders" in person_info:
                 elapsed_time = time.time() - person_info["time"]
-                waiting_time_str = time.strftime('%H:%M:%S', time.gmtime(elapsed_time))
+                waiting_time_str = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
                 x, y, w, h = person_info["bbox"]
-                cv2.putText(frame, f'Person {person_info["id"]} - Waiting Time: {waiting_time_str}',
-                            (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, colors_dict[person_info["id"]], 2)
-                cv2.circle(frame, person_info["centroid"], 5, colors_dict[person_info["id"]], -1)
+                cv2.putText(
+                    frame,
+                    f'Person {person_info["id"]} - Waiting Time: {waiting_time_str}',
+                    (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    colors_dict[person_info["id"]],
+                    2,
+                )
+                cv2.circle(
+                    frame,
+                    person_info["centroid"],
+                    5,
+                    colors_dict[person_info["id"]],
+                    -1,
+                )
 
         ret, buffer = cv2.imencode(".jpg", frame)
         frame = buffer.tobytes()
         yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
